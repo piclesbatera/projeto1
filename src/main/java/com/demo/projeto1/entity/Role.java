710a116ff3dafb648f4e61d90882c353f48efb81
Péricles Rodrigues <prodrigues_beijaflore@timbrasil.com.br>
2022-02-22 17:38:54 -0300

    first commit

---
+++ src/main/java/com/demo/projeto1/entity/Role.java
@@ -0,0 +1,31 @@
+package com.demo.projeto1.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
